de21483b3d38154e2364ca7f72121a37c2b0726b
Prithvi Anil K <thepirateking3d2y@gmail.com>
2021-05-27 23:12:48 +0530

    deploy test #1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
